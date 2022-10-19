078db23d5f06be05a44767688b711d8c8b7ba605
SadIrina <115239794+SadIrina@users.noreply.github.com>
2022-10-19 17:05:14 +0300

    Vers 1910

--- homework.py
+++ homework.py
@@ -4,7 +4,7 @@ from typing import ClassVar, Type
 
 @dataclass(init=True, repr=False, eq=False,
            order=False, unsafe_hash=False,
-           frozen=False, kw_only=False, slots=False)
+           frozen=False, slots=False)
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     training_type: str  # вид тренировки
